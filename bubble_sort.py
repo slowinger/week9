439f3f66f69902572a5f378e9c60f0794c50e076
slowinger <slowinger@uchicago.edu>
2015-03-04 18:35:06 -0600

    added solution to bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,20 @@
 import random
 
+
 def sort(items):
-  # 1. TO DO: Implement a "bubble sort" routine here
-  pass
+    stop = False
+    while stop is False:
+        for i in range(len(items) - 1):
+            if i == 0:
+                swaps = 0
+            if items[i] > items[i + 1]:
+                items[i], items[i + 1] = items[i+1], items[i]
+                swaps += 1
+        if swaps == 0:
+            stop = True
+    return items
+
+
 
 
 numbers = list(range(10))
@@ -13,4 +25,4 @@ print("The list was sorted correctly!")
 
 # 2. Change this print statement to display the complexity category.
 #    Refer to the cheat sheet in week9-class for examples.
-print("This algorithm is classified as: O(....)")
+print("This algorithm is classified as: O(n^2)")
